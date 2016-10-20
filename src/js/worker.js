26677175f37d3140f38d52ac3e0011454aae726a
Dmytro Mytsko <dmytro@deepinspire.com>
2016-10-20 18:21:16 +0300

    Initial commit

---
+++ src/js/worker.js
@@ -0,0 +1,33 @@
+console.log('worker thread');
+
+onmessage = function(event) {
+	console.log('worker:onmessage:', event.data.action);
+	switch(event.data.action) {
+		case 'init':
+			var arr = [];
+
+			var n = 100000;
+
+			var startTime = +new Date();
+			for(var i = 0; i < n; i++) {
+				arr.push({index:i, name: "name_"+i});
+			}
+			var endTime = +new Date() - startTime;
+
+			postMessage({action: 'init', arr: arr, initTimeArray: endTime,startTime: +new Date()});
+			break;
+		case 'sendArray':
+			console.log('worker:thread:onmessage:sendArray', +new Date() - event.data.startTime, event.data.initTimeArray, event.data);
+			break;
+		default:{}
+	}
+};
+
+/*var i = 0;
+function timedCount() {
+	i = i + 1;
+	postMessage({index:i, arr:[]});
+	setTimeout("timedCount()",500);
+}
+
+timedCount();*/
